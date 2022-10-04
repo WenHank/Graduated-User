8b6b5e7e84cb0f6ceea5309abec560db3db7e443
Wen <93903888+WenHank@users.noreply.github.com>
2022-10-04 21:08:29 +0800

    new

--- src/App.js
+++ src/App.js
@@ -3,163 +3,127 @@ import { db } from "./firebase";
 import { uid } from "uid";
 import { set, ref, onValue, remove, update } from "firebase/database";
 import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css"; // or include from a CDN
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
+import RangeSlider from "react-bootstrap-range-slider";
+import styled from "@emotion/styled";
+import { v4 as uuidv4 } from "uuid";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function App() {
-  const [red, setRed] = useState(244);
-  const [green, setGreen] = useState(244);
-  const [blue, setBlue] = useState(244);
-  const [song, setSong] = useState("");
+  const [video, setVideo] = useState("");
   const [isEdit, setIsEdit] = useState(false);
-  const [rgbUuid, setRgbUuid] = useState("06f2462716c");
-  const [songUuid, setSongUuid] = useState("39822c4b746");
-
-  const handleRedcahnge = (e) => {
-    setRed(e.target.value);
-  };
-  const handleGreencahnge = (e) => {
-    setGreen(e.target.value);
-  };
-  const handleBluecahnge = (e) => {
-    setBlue(e.target.value);
-  };
-
+  const [frequenceUuid, setFrequenceUuid] = useState("06f2462716c");
+  const [vedioUuid, setVedioUuid] = useState("39822c4b746");
+  const [pageUuid, setPageUuid] = useState("f23c20b0339");
+  const [page, setPage] = useState();
+  //read
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      Object.values(data).map((obj) => {
+        if (obj.uuid === "f23c20b0339") {
+          setPage(obj.thePage);
+        }
+      });
+    });
+  }, []);
   //write
   // const writeToDataBase = () => {
   //   const uuid = uid();
   //   set(ref(db, `/${uuid}`), {
-  //     song,
+  //     page,
   //     uuid,
   //   });
   // };
 
-  //Update
-  const handleSubmitChange = () => {
-    update(ref(db, `/${rgbUuid}`), {
-      red,
-      green,
-      blue,
-    });
-    update(ref(db, `/${songUuid}`), {
-      song,
-    });
-    setIsEdit(false);
-    setSong("");
-    setBlue(244);
-    setGreen(244);
-    setRed(244);
+  //range slider
+  const Part1 = () => {
+    const [noise, setNoise] = useState(0);
+    const [frequence, setFrequence] = useState(0);
+
+    //Update
+    const handleSubmitChange = () => {
+      update(ref(db, `/${frequenceUuid}`), {
+        frequence,
+        noise,
+      });
+      setIsEdit(false);
+      setVideo("");
+    };
+    const handleChangePage = () => {
+      let thePage = 2;
+      update(ref(db, `/${pageUuid}`), {
+        thePage,
+      });
+    };
+    return (
+      <div className="col-class">
+        <h3>第一部分</h3>
+        <div className="row-class" style={{ marginBottom: "30px" }}>
+          <div style={{ marginRight: "10px" }}>頻率</div>
+          <RangeSlider
+            value={frequence}
+            onChange={(changeEvent) => setFrequence(changeEvent.target.value)}
+          />
+        </div>
+        <div className="row-class">
+          <div style={{ marginRight: "10px" }}>聲音</div>
+          <RangeSlider
+            value={noise}
+            onChange={(changeEvent) => setNoise(changeEvent.target.value)}
+          />
+        </div>
+        <img
+          className="pageImg1"
+          src="../Img/page.png"
+          onClick={handleChangePage}
+        />
+        <button onClick={handleSubmitChange}>Submit</button>
+      </div>
+    );
   };
 
-  //Delete
-  const handleDelete = (todo) => {
-    remove(ref(db, `/${todo.uuid}`));
+  const Part2 = () => {
+    //Update
+    let theVideo = video;
+    const handleSubmitChange = () => {
+      update(ref(db, `/${vedioUuid}`), {
+        theVideo,
+      });
+    };
+    const handleChangePage = () => {
+      let thePage = 1;
+      update(ref(db, `/${pageUuid}`), {
+        thePage,
+      });
+    };
+    return (
+      <div className="col-class">
+        <h3>第二部分</h3>
+        <input value={video} disabled />
+        <div className="row-class">
+          <button className="vedio" onClick={() => setVideo("part1")}>
+            Pasrt1
+          </button>
+          <button className="vedio" onClick={() => setVideo("part2")}>
+            Pasrt2
+          </button>
+        </div>
+
+        <img
+          className="pageImg2"
+          src="../Img/page.png"
+          onClick={handleChangePage}
+        />
+        <button onClick={handleSubmitChange}>Submit</button>
+      </div>
+    );
   };
   return (
-    <div
-      className="App"
-      style={{ background: `rgb(${red}, ${green}, ${blue})` }}
-    >
+    <div className="App">
       <h1>User</h1>
-
-      <div className="InputCon">
-        <p>Red</p>
-        <input type="text" value={red} onChange={handleRedcahnge}></input>
-      </div>
-      <div className="InputCon">
-        <p>Green</p>
-        <input type="text" value={green} onChange={handleGreencahnge}></input>
-      </div>
-      <div className="InputCon">
-        <p>Blue</p>
-        <input type="text" value={blue} onChange={handleBluecahnge}></input>
-      </div>
-      <div className="InputCon">
-        <p>Vedio</p>
-        <input type="text" disabled value={song}></input>
-      </div>
-      <div className="row-class">
-        <button
-          onClick={() => {
-            setSong("part1");
-          }}
-        >
-          part1
-        </button>
-        <button
-          onClick={() => {
-            setSong("part2");
-          }}
-        >
-          part2
-        </button>
-      </div>
-      {/* <div className="col-class">
-        <div className="row-class">
-          <div>
-            <img
-              className="albumImg"
-              src="../Img/500.jpeg"
-              onClick={() => {
-                setSong("500");
-              }}
-            />
-            <h3>被動</h3>
-          </div>
-          <div>
-            <img
-              className="albumImg"
-              src="../Img/JayChou.jpeg"
-              onClick={() => {
-                setSong("JayChou");
-              }}
-            />
-            <h3>雙截棍</h3>
-          </div>
-          <div>
-            <img
-              className="albumImg"
-              src="../Img/JayPark.jpeg"
-              onClick={() => {
-                setSong("JayPark");
-              }}
-            />
-            <h3>All I Wanna Do</h3>
-          </div>
-        </div>
-        <div className="row-class">
-          <div>
-            <img
-              className="albumImg"
-              src="../Img/kanaria.jpeg"
-              onClick={() => {
-                setSong("kanaria");
-              }}
-            />
-            <h3>酔いどれ知らず</h3>
-          </div>
-          <div>
-            <img
-              className="albumImg"
-              src="../Img/ShawnMendes.jpeg"
-              onClick={() => {
-                setSong("ShawnMendes");
-              }}
-            />
-            <h3>When You're Gone</h3>
-          </div>
-          <div>
-            <img
-              className="albumImg"
-              src="../Img/Maroon5.jpeg"
-              onClick={() => {
-                setSong("Maroon5");
-              }}
-            />
-            <h3>Payphone</h3>
-          </div>
-        </div>
-      </div> */}
-
-      <button onClick={handleSubmitChange}>Submit</button>
+      {page === 1 ? <Part1 /> : <Part2 />}
     </div>
   );
 }
